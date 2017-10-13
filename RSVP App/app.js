39cfba3b02cdc795bfb33a1ca31d6bfe0bda2a28
George V <georgevuong18@gmail.com>
2017-10-13 09:09:40 -0700

    remove functionality per list item

--- RSVP App/app.js
+++ RSVP App/app.js
@@ -4,14 +4,10 @@ const form = document.getElementById("registrar");
 const input = form.querySelector("[type=text]");
 const ul = document.getElementById("invitedList");
 
-// `submit` event fires when user either clicks Submit or hits Enter on the form element, not `button` or `submit input`.
-form.addEventListener("submit", (e) => {
-  e.preventDefault(); // without this line, page reloads when HTML form is submitted. Browser sends info to the URL specified by action attribute and loads that URL as well.
-  console.log(input.value)
+// create list element
+function createLi(invitee) {
   const li = document.createElement('li');
-  li.textContent = input.value;
-  input.value = '';
-  ul.appendChild(li);
+  li.textContent = invitee;
 
   const label = document.createElement('label');
   label.textContent = 'Confirmed';
@@ -19,17 +15,43 @@ form.addEventListener("submit", (e) => {
 
   const checkboxInput = document.createElement('input');
   checkboxInput.type = 'checkbox';  label.appendChild(checkboxInput);
+
+  const removeButton = document.createElement('button');
+  removeButton.textContent = "remove";
+  li.appendChild(removeButton);
+
+  return li;
+}
+
+// `submit` event fires when user either clicks Submit or hits Enter on the form element, not `button` or `submit input`. Argument, 'e', can be any name.
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // prevents page reloading when HTML form is submitted, in which the browser sends info to URL specified by action attribute and loads that URL as well.
+  const invitee = input.value;
+  input.value = '';
+  const li = createLi(invitee);
+  ul.appendChild(li);
 });
 
-// Instead of adding an event handler to each checkbox created, add a single event handler to just one element, <ul>. Due to event bubbling, an event that occurs on one element (i.e. checkboxInput) bubbles up to its parent (i.e. <label>) or other ancestors (i.e. <ul>).
-// Also, "change" event is used instead of "click" to see if checkbox state has changed from unchecked to checked, and vice versa, not so much as to whether the box is clicked.
-ul.addEventListener("click", (e) => {
-  const checkboxInput = e.target;
-  const checked = checkboxInput.checked;
-  const listItem = checkboxInput.parentNode.parentNode;
+// Instead of adding an event handler to each checkbox created, add a single event delegated handler to just one element, <ul>. Due to event bubbling, an event that occurs on one element (i.e. checkboxInput) bubbles up to its parent (i.e. <label>) or other ancestors (i.e. <ul>). Due to event delegation, the action is delegated to its children.
+
+// Also, "change" event is used instead of "click" to see if checkbox state has changed from unchecked to checked, and vice versa, not so much as to whether the box is clicked. "Change" event is fired when an <input>, <select>, or <textarea> value has changed.
+ul.addEventListener("change", (e) => {
+  const checkboxInput = e.target; // returns element that initiated the event, which is the checkbox input element
+  const checked = checkboxInput.checked; // returns a boolean
+  const listItem = checkboxInput.parentNode.parentNode; // DOM traversal in action
   if (checked) {
     listItem.className = "responded";
   } else {
     listItem.className = '';
+  }
+})
+
+// use a delegated handler on the parent element to receive the removeButton click event because user may add and remove lots of names to list.
+ul.addEventListener("click", (e) => {
+  const removeButton = e.target; // returns element that initiated the event, which is the checkbox button element
+  if (e.target.textContent === 'remove') {
+    const li = removeButton.parentNode;
+    const ul = li.parentNode;
+    ul.removeChild(li);
   }
 })
