757d8e3e9286dfc53045336f2ebf62e29eb99323
George V <georgevuong18@gmail.com>
2017-10-16 10:53:56 -0700

    create list of JSX elements in App, ensure .gitignore works

--- ScoreboardApp/app.jsx
+++ ScoreboardApp/app.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-
+var PLAYERS = [
+  {
+    name: "George V.",
+    score: 12,
+  },
+  {
+    name: "George Foreman",
+    score: 2,
+  },
+  {
+    name: "George St. Pierre",
+    score: 1,
+  },
+]
 function Header(props) {
   return (
     <div className="header">
@@ -52,13 +65,23 @@ function App(props) {
     <div className="scoreboard">
       <Header title={props.title} />
       <div className="players">
-        <Player name="George V." score={13 - 1} />
-        <Player name="George Foreman" score={23} />
-        <Player name="George St.Pierre" score={4} />
+        {props.players.map(function(player, id) {
+          return <Player name={player.name} score={player.score} key={id}/>
+        })}
       </div>
     </div>
   );
 }
+
+// Players is an array of objects with the following shape, that needs to be passed in. title is optional.
+App.propTypes = {
+  title: PropTypes.string,
+  players: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    score: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+  })).isRequired,
+};
 
 App.defaultProps = {
   title: "My Scoreboard",
@@ -66,7 +89,13 @@ App.defaultProps = {
 
 // <App /> creates an instance of the App component in JSX
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(<App players={PLAYERS}/>, document.getElementById('root'));
 });
 
 // first arg in ReactDOM.render() is virtualDOM element, second arg is real DOM element where we want to place our virtual DOM. Above code says to create an <App /> instance (or React element) and render within a DOM element called "root" on the page.
+
+// Remember, React keeps a copy of the previous Virtual DOM. A unique key is required to help React understand which object maps to which virtual DOM element. So if item in list is rearranged, added, or deleted, React can use key to reorder list as opposed to changing the content for each DOM Node (or item) based on position in list.
+
+// Cannot use JS for loop to iterate over items inside JSX expression. Need to use .map().
+
+// Break component into smaller components when component has too much markup, a component does too many things, or when component is reused.
