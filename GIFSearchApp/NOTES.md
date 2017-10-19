6f73fa72506e2d01dcd7e5ab3e2fa74b19326b64
George V <georgevuong18@gmail.com>
2017-10-19 11:25:59 -0700

    use axios to fetch trending data from giphyAPI

--- GIFSearchApp/NOTES.md
+++ GIFSearchApp/NOTES.md
@@ -1,4 +1,4 @@
 * React is just a view library, and it's only concern is rendering components using state and properties passed to them. React doesn't have a built in way to fetch data from a server. React doesn't know there is a server.
 * There are JS methods used to fetch data to React app. Both of the following methods use promise to handle results we get back from the server:
-  * Fetch API: A modern replacement for XMLHTTPRequest, it is a data fetching interface that's native to the browser. 
-  * Axios: Similar to Fetch API.
+  * Fetch API: A modern replacement for XMLHTTPRequest, it is a data fetching interface that's native to the browser.
+  * Axios: Similar to Fetch API, but has unique features. This library isn't specific to React. Node.js app. Supports promiseAPI, and automatically converts data to JSON. Protects against CSRF attacks. 
--- GifSearchApp/App.jsx
+++ GifSearchApp/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchForm from './src/components/SearchForm';
 import GifList from './src/components/GifList';
-import 'whatwg-fetch';
+import axios from 'axios';
+import 'whatwg-fetch'; // used to support old browsers when using fetchAPI
+
 
 export default class App extends Component {
   constructor() {
-    super(); // Calling super() allows us to user "this" inside constructor within context of App class rather than parent Component class extending from React.
+    super(); // Calling super() allows us to use "this" inside constructor within context of App class, rather than parent Component class extending from React.
     this.state = {
       gifs: [],
     };
@@ -13,14 +15,15 @@ export default class App extends Component {
 
   // method runs immediately after component is added to DOM. Convenient for loading external data because at this point in the lifecycle, component has a DOM representation
   componentDidMount() {
-    // fetchAPI returns a JS promise, and if promise is "fulfilled", which is when browser receives data from the server, .then() methods which store callbacks will get executed
-    fetch("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=24")
-    .then(response => response.json())
-    .then(responseData => this.setState({ gifs: responseData.data }))
-    // if promise is not "fulfilled" and gets rejected (i.e. you're offline, or API server is down), use catch() to "catch" or handle any errors.
-    .catch(error => {
-      console.log("Error fetching and parsing data: ", error);
-    })
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=24')
+      .then(response =>
+        this.setState({ gifs: response.data.data, }) // response.data is response provided by server already in JSON format. Append .data to access GiphyAPI data array.
+      )
+      .catch(error => {
+        console.log("Error fetching and parsing data: ", error);
+      });
+
+    // To use FetchAPI method: refer to code at bottom of page
   }
 
   render() {
@@ -40,3 +43,14 @@ export default class App extends Component {
     );
   }
 }
+
+// fetchAPI returns a JS promise, and if promise is "fulfilled", which is when browser receives data from the server, .then() methods which store callbacks will get executed
+// fetch("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=24")
+// .then(response => response.json())
+// .then(responseData =>
+//   this.setState({ gifs: responseData.data })
+// )
+// // if promise is not "fulfilled" and gets rejected (i.e. you're offline, or API server is down), use catch() to "catch" or handle any errors.
+// .catch(error =>
+//   console.log("Error fetching and parsing data: ", error)
+// );
