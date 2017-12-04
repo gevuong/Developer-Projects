8a688a33c8163a36e48839c98090fa016f3a1258
George V <georgevuong18@gmail.com>
2017-12-03 22:16:32 -0800

    t-shape piece drops at 1sec interval

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -7,21 +7,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const ctx =  canvasEl.getContext('2d'); // set 2d rendering context for the drawing surface of <canvas< element.
   ctx.scale(20, 20);
-  ctx.fillStyle = "#000";
-  ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
-  // fillRect(x, y, width, height), x: x-comp of coord for rectangle's starting point, y: y-comp of coord for rectangle's starting point
 
   // T-shaped piece
   const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0]
-  ]
+  ];
 
-  function draw() {
-    drawMatrix(matrix, offset)
-  }
-
+  // draws T-shaped piece by filling in color for each element in matrix
   function drawMatrix(matrix, offset) { // offset used to move piece
     matrix.forEach((row, y) => {
       row.forEach((val, x) => {
@@ -33,7 +27,36 @@ document.addEventListener("DOMContentLoaded", function() {
     })
   }
 
-  // drawMatrix(matrix, {x: 1, y: 1});
-  function update()
-  draw();
+  const player = {
+    pos: {x: 2, y: 2},
+    matrix: matrix,
+  }
+
+  window.player = player; // test if piece moves by altering pos.x and pos.y
+
+  function draw() {
+    // redraws ctx every time frame is updated to remove previously rendered piece
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+    // fillRect(x, y, width, height), x: x-comp of coord for rectangle's starting point, y: y-comp of coord for rectangle's starting point
+    drawMatrix(player.matrix, player.pos);
+  }
+
+  let dropCounter = 0;
+  let dropInterval = 1000;
+  let prevTime = 0;
+  function update(time = 0) {
+    dropCounter += time - prevTime;
+    prevTime = time;
+    console.log(dropCounter);
+
+    if (dropCounter >= dropInterval) {
+      player.pos.y += 1;
+      dropCounter = 0;
+    }
+    draw();
+    requestAnimationFrame(update); // takes a callback as an arg to be invoked before repaint. callback itself calls requestAnimationFrame() to animate another frame before repaint. call method when ready to update animation onscreen
+  }
+
+  update();
 })
