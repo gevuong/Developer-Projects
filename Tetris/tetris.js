2e1021a6e6385c5d26d17535e7aa6f3ffa952ec1
George V <georgevuong18@gmail.com>
2017-12-04 14:09:16 -0800

    add collision detection and merge fcn to map piece to board when piece reaches bottom of board

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -2,37 +2,126 @@
 document.addEventListener("DOMContentLoaded", function() {
   const canvasEl = document.getElementById("tetris");
   // set height and width attributes of canvasEl
-  canvasEl.width = 500;
-  canvasEl.height = 500;
+  canvasEl.width = 240;
+  canvasEl.height = 400;
 
   const ctx =  canvasEl.getContext('2d'); // set 2d rendering context for the drawing surface of <canvas< element.
   ctx.scale(20, 20);
 
-  // T-shaped piece
+  // CONSTANTS
   const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0]
   ];
 
-  // draws T-shaped piece by filling in color for each element in matrix
-  function drawMatrix(matrix, offset) { // offset used to move piece
+  const player = {
+    pos: {x: 5 , y: 0},
+    matrix: matrix,
+  };
+  window.player = player; // test if piece moves by altering pos.x and pos.y
+
+  const board = createMatrix(12, 20) // 12 units wide, 20 units high
+  console.log(board); console.table(board);
+  window.board = board;
+
+
+  // draw T-shaped piece by filling in color for each element in matrix
+  function drawMatrix(matrix, offset) { // offset used to move and redraw block
     matrix.forEach((row, y) => {
       row.forEach((val, x) => {
-        if (val !== 0) { // draw red rectangle of width 1, height 1
+        if (val !== 0) { // draw red square of width 1, height 1
           ctx.fillStyle = 'red';
           ctx.fillRect(x + offset.x, y + offset.y, 1, 1);
         }
-      })
-    })
+      });
+    });
   }
 
-  const player = {
-    pos: {x: 2, y: 2},
-    matrix: matrix,
+  // used to create tetris board
+  function createMatrix(width, height) {
+    const matrix = [];
+    while (height--) { // 0, -0, "", '', null, undefined, NaN, and false return falsey values in JS
+      matrix.push(new Array(width).fill(0));
+    }
+    return matrix;
+    // alternative:
+    // matrix = new Array(height).fill(0).map(row => (new Array(width).fill(0)))
   }
 
-  window.player = player; // test if piece moves by altering pos.x and pos.y
+
+  function collide(board, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for (var row = 0; row < m.length; ++row) {
+      for (var col = 0; col < m[row].length; ++col) {
+        if (m[row][col] !== 0 &&
+          (board[row + o.y] && board[row + o.y][col + o.x]) !== 0) {
+            console.log("true");
+          return true;
+        }
+      }
+    }
+    console.log("false");
+    return false;
+  }
+
+  // function collide(board, player) {
+  //   const [matrix, offset] = [player.matrix, player.pos];
+  //   matrix.forEach((row, y) => { // for (y = 0, y < matrix.length)
+  //     row.forEach((val, x) => {
+  //       if (matrix[y][x] !== 0 && // if player.matrix element is a block AND...
+  //         (board[y + offset.y] && board[y + offset.y][x + offset.x]) !== 0) { // if board has a row, y, AND...board has a col, x, and both are not 0, there is a collision. If row y alone, doesn't exist, it is also a collision because it's not equal to 0.
+  //         console.log("true");
+  //         return true;
+  //       }
+  //     });
+  //   });
+  //   console.log("false");
+  //   return false;
+  // }
+
+  // copy values from currentPiece/player to board
+  function merge(board, player) {
+    player.matrix.forEach((row, y) => {
+      row.forEach((val, x) => {
+        if (val !== 0) {
+          board[y + player.pos.y][x + player.pos.x] = val;
+        }
+      });
+    });
+  }
+  window.merge = merge;
+
+  function moveDirection(dir) {
+    player.pos.x += dir;
+    if (collide(board, player)) {
+      player.pos.x -= dir;
+    }
+  }
+
+  function playerDrop() {
+    player.pos.y++;
+    if (collide(board, player)) {
+      player.pos.y--;
+      merge(board, player);
+      player.pos.y = 0; // when piece reaches bottom of board, piece starts from the top
+    }
+    dropCounter = 0; // don't want drop to happen immediately after arrowDown
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.keyCode === 39) { // arrowRight
+      moveDirection(1);
+      console.log(player.pos.x);
+    } else if (e.keyCode === 37) { // arrowLeft
+      moveDirection(-1);
+      console.log(player.pos.x);
+    } else if (e.keyCode === 40) { // arrowDown
+      playerDrop();
+      console.log(player.pos.y);
+    }
+  });
+
 
   function draw() {
     // redraws ctx every time frame is updated to remove previously rendered piece
@@ -40,20 +129,22 @@ document.addEventListener("DOMContentLoaded", function() {
     ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
     // fillRect(x, y, width, height), x: x-comp of coord for rectangle's starting point, y: y-comp of coord for rectangle's starting point
     drawMatrix(player.matrix, player.pos);
+    drawMatrix(board, {x: 0, y: 0});
   }
+
 
   let dropCounter = 0;
   let dropInterval = 1000;
   let prevTime = 0;
+
   function update(time = 0) {
     dropCounter += time - prevTime;
     prevTime = time;
-    console.log(dropCounter);
 
     if (dropCounter >= dropInterval) {
-      player.pos.y += 1;
-      dropCounter = 0;
+      playerDrop();
     }
+
     draw();
     requestAnimationFrame(update); // takes a callback as an arg to be invoked before repaint. callback itself calls requestAnimationFrame() to animate another frame before repaint. call method when ready to update animation onscreen
   }
