af97342b7ac59588123567916007ae3b7e54fc15
George V <georgevuong18@gmail.com>
2017-12-03 20:30:37 -0800

    init commit tetris

---
+++ Tetris/tetris.js
@@ -0,0 +1,10 @@
+// 
+document.addEventListener("DOMContentLoaded", function() {
+  const canvasEl = document.getElementById("tetris");
+  // set height and width attributes of canvasEl
+  canvasEl.width = 500;
+  canvasEl.height = 500;
+
+  const ctx =  canvasEl.getContext('2d'); // set 2d rendering context for the drawing surface of <canvas< element.
+
+})
