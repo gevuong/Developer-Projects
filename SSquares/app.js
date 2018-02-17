a9b1fdb939c3a775bd1c167602e20453af5b555b
George V <georgevuong18@gmail.com>
2018-02-17 02:35:54 -0800

    fix eventListener bug to change backgroundColor on first click

--- SSquares/app.js
+++ SSquares/app.js
@@ -3,20 +3,39 @@ document.addEventListener("DOMContentLoaded", () => {
   let stack = [];
 
   Array.from(squares).forEach(square => {
-    square.addEventListener("click", (e) => {
+    square.style.backgroundColor = "white";
+
+    square.addEventListener("click", function(e) {
       const clickedSquare = e.target;
-      console.log(clickedSquare);
-      // if (!stack.includes(clickedSquare)) {
-      //   stack.push(clickedSquare);
-      // }
+      // console.log(clickedSquare);
+
       if (clickedSquare.style.backgroundColor === "white") {
         clickedSquare.style.backgroundColor = "#33C6E4";
-        stack.push(clickedSquare);
+        // To prevent the same square from adding onto stack
+        if (!stack.includes(clickedSquare)) {
+          stack.push(clickedSquare);
+        }
         console.log("stack: ", stack);
       } else {
         clickedSquare.style.backgroundColor = "white";
       };
+
+      if (stack.length === 9) {
+        for (let square = 0; square < stack.length; square++) {
+          let poppedSquare = stack.pop();
+          console.log("poppedSquare: ", poppedSquare);
+          setTimeout(changeBackgroundColor(poppedSquare), 300);
+        };
+      }
+
     });
+
   });
+
+  window.stack = stack;
+
+  function changeBackgroundColor(square) {
+    square.style.backgroundColor = "white";
+  };
 
 });
