e000911086ee4d57b7a7580d2dd61ae1b2c9b872
George V <georgevuong18@gmail.com>
2018-02-22 16:20:35 -0800

    complete print_items_per_interval function

--- item_counter/README.md
+++ item_counter/README.md
@@ -1,3 +1,10 @@
+#README
+
+###Assumptions
+1. All times are within same date.
+2. items are default to be singular, not pluralized.
+3. 
+
 External Resources
 
 ###When should you use DateTime and when should you use Time?
--- item_counter/test.rb
+++ item_counter/test.rb
@@ -1,26 +1,28 @@
+require 'active_support/inflector'
+
 class ItemCounter
-  attr_accessor :item_count, :rides
+  attr_reader :item_counts, :rides
 
   def initialize
-    @item_count = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+    @item_counts = Hash.new { |hash, key| hash[key] = Hash.new(0) }
     @rides = []
   end
 
-
   def process_ride(ride)
-    # Step 0: push ride object into rides array. Whenever #process_ride is executed, each ride's basket_items is reconsidered due to new time intervals
+    # Step 0: push ride object into ivar @rides whenever a ride object is processed. This enables each ride object to be considered every time a new ride object is processed
     @rides.push(ride)
-    @item_count = Hash.new { |hash, key| hash[key] = Hash.new(0) }
+    @item_counts = Hash.new { |hash, key| hash[key] = Hash.new(0) }
 
-    # Step 1: store start_time and end_time of all rides as keys in hash
+    # Step 1: store start_time and end_time of all processed rides as keys in hash
     @rides.each do |ride_obj|
-      next if ride_obj.bike_basket_items.empty?
-      @item_count[ride_obj.start_time]
-      @item_count[ride_obj.end_time]
+      next if ride_obj.bike_basket_items.empty? # excludes time intervals during which no items were in transit
+
+      @item_counts[ride_obj.start_time]
+      @item_counts[ride_obj.end_time]
     end
 
     # Step 2: iterate through times array and check if time is within time range of ride object
-    times = @item_count.keys # returns array
+    times = @item_counts.keys # returns array
 
     @rides.each do |ride_obj|
       start_time = ride_obj.start_time
@@ -30,18 +32,40 @@ class ItemCounter
       times.each do |time|
         if time.between?(start_time, end_time - 60)
           basket_items.each do |item, quantity|
-            @item_count[time][item] += quantity
+            @item_counts[time][item] += quantity
           end
-          # @item_count
         end
       end
     end
-
-    # @item_count
   end
 
+
   def print_items_per_interval
-    @item_count
+    # Step 0: sort hash based on time
+    times = []
+    items_counts = []
+
+    @item_counts.sort.each do |item_count|
+      time = item_count[0].strftime("%I:%M")
+      times.push(time)
+      items = item_count[1] # returns hash
+
+      # puts time
+      str = ""
+      items.sort.each do |sub_arr|
+        item = sub_arr[0]
+        quantity = sub_arr[1]
+        item = item.pluralize if quantity > 1
+        str.concat("#{quantity} #{item}, ")
+      end
+      items_counts.push(str.slice(0...-2))
+    end
+
+    (0...times.length - 1).each do |idx|
+
+      return puts "#{times[idx]}-#{times[idx + 1]} -> #{items_counts[idx]}" if idx == times.length - 2
+      puts "#{times[idx]}-#{times[idx + 1]} -> #{items_counts[idx]}"
+    end
   end
 end
 
@@ -59,22 +83,20 @@ end
 
 
 # Test cases
-# to create Ride instance
+# create Ride instances
 ride_1 = Ride.new(Time.new(2018, 2, 19, 7, 0, 0), Time.new(2018, 2, 19, 7, 30, 0), { "apples" => 2, "brownies" => 1 })
 ride_2 = Ride.new(Time.new(2018, 2, 19, 7, 10, 0), Time.new(2018, 2, 19, 8, 0, 0), { "apples" => 1, "carrots" => 3 })
 ride_3 = Ride.new(Time.new(2018, 2, 19, 7, 20, 0), Time.new(2018, 2, 19, 7, 45, 0), { "apples" => 1, "brownies" => 2, "diamonds" => 4 })
+
+# no items in transit
 ride_4 = Ride.new(Time.new(2018, 2, 19, 8, 0, 0), Time.new(2018, 2, 19, 8, 15, 0), {})
 
-# item_counter = ItemCounter.new([ride_1, ride_2, ride_3, ride_4])
 
 item_counter = ItemCounter.new
 item_counter.process_ride(ride_1)
-# # p item_counter.item_count
-#
 item_counter.process_ride(ride_2)
-# # p item_counter.item_count
 item_counter.process_ride(ride_3)
 # p item_counter.rides
+# p item_counter
 
-# item_counter.process_ride
-p item_counter.item_count
+p item_counter.print_items_per_interval
