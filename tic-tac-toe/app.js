1e75a2bf581487ca59db5088b6aba92ff53e45dd
George V <georgevuong18@gmail.com>
2018-04-16 15:43:04 -0700

    renders x or o each turn

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,4 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const mainDiv = document.getElementById("main");
-    
+    const squares = document.getElementsByClassName("square");
+    const squaresArray = Array.from(squares);
+    let current = "x";
+
+    squaresArray.forEach(square => {
+        square.addEventListener("click", (e) => {
+            console.log("innerHTML: ", e.target);
+            console.log("innerHTML: ", e.target.innerHTML);
+            if (e.target.innerHTML === "") {
+                e.target.innerHTML = current;
+                current = (current === "x" ? "o" : "x");
+            }
+        })
+    })
 });
