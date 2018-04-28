ca4f93eeba343636ddecc06c7c181a7aedcee408
George V <georgevuong18@gmail.com>
2018-04-28 15:33:27 -0700

    complete 6 of 8 problems in part 6 of sqlzoo

--- SQLZoo/lib/04a_countries_bonus.rb
+++ SQLZoo/lib/04a_countries_bonus.rb
@@ -14,22 +14,69 @@ require_relative './sqlzoo.rb'
 # functions. Attempt them after completing section 05.
 
 def highest_gdp
-  # Which countries have a GDP greater than every country in Europe? (Give the
-  # name only. Some countries may have NULL gdp values)
-  execute(<<-SQL)
-  SQL
+	# Which countries have a GDP greater than every country in Europe? (Give the
+	# name only. Some countries may have NULL gdp values)
+	execute(<<-SQL)
+		SELECT 
+			name
+		FROM
+			countries 
+		GROUP BY
+			name
+
+	SQL
+
+
+	# hm, lets break this problem down. First obtain all countries in Europe 
+	execute(<<-SQL)
+		SELECT
+			name 
+		FROM
+			countries 
+		WHERE 
+			continent = 'Europe'
+	SQL
+
+	# ok, now how do we compare each country's gdp with every country's gdp in Europe?
+
 end
+
+p highest_gdp 
 
 def largest_in_continent
-  # Find the largest country (by area) in each continent. Show the continent,
-  # name, and area.
-  execute(<<-SQL)
-  SQL
+	# Find the largest country (by area) in each continent. Show the continent,
+	# name, and area.
+	execute(<<-SQL)
+		SELECT 
+			continent, name, area 
+		FROM
+			countries 
+		GROUP BY
+			continent 
+		ORDER BY 
+			area DESC
+		LIMIT
+			1
+	SQL
 end
 
+p largest_in_continent 
+
 def large_neighbors
-  # Some countries have populations more than three times that of any of their
-  # neighbors (in the same continent). Give the countries and continents.
-  execute(<<-SQL)
-  SQL
+	# Some countries have populations more than three times that of any of their
+	# neighbors (in the same continent). Give the countries and continents.
+	execute(<<-SQL)
+	/*	SELECT 
+			name, continent 
+		FROM
+			countries 
+		WHERE 
+			population > 3 * (
+				SELECT
+					population
+				FROM 
+					countries 
+				
+			)*/
+	SQL
 end
