b1d2407b2803487edf19224f859ea2c84f669d4e
George V <georgevuong18@gmail.com>
2018-05-05 13:01:08 -0700

    initialize headlight

--- car_repair.rb
+++ car_repair.rb
@@ -54,27 +54,41 @@ class FrontBumperCover
     end 
 end 
 
-class FrontFender 
+class HeadLight 
+    def initialize(side = 'left')
+        @side = side
+        @oem_cost = 938.57 # bi-xenon w/out adaptive headlamp
+        @oem_two_cost = 1096.95 # bi-xenon w/ adaptive headlamp
+        @ebay_cost = 592.95 # bi-xenon w/out adaptive headlamp
+        @oem_link = 'http://www.realoem.com/bmw/enUS/showparts?id=VB13-USA-02_2004_E90_BMW_325i&diagId=63_0907#63117161665'
+        @ebay_link = 'https://www.ebay.com/itm/For-BMW-325i-328i-330i-335i-xi-Bi-Xenon-Hella-Left-Side-Headlight-Assembly/323177359364?fits=Year%3A2006%7CMake%3ABMW%7CModel%3A325i%7CSubmodel%3ABase%7CEngine+-+Liter_Display%3A3.0L%7CTrim%3ABase+Sedan+4-Door%7CEngine%3A3.0L+2996CC+l6+GAS+DOHC+Naturally+Aspirated&epid=2235313231&hash=item4b3edf2804:g:gjsAAOSwUN9avOtt&vxp=mtr'
+    end 
+end 
+
+class FrontSidePanel 
     def initialize(side = 'left')
         @body_nut_count = 8
-        @cost = 334.17 # USD from realoem
+        @hexbolt_with_washer_count = 16
+        @oem_cost = 334.17 # USD from realoem
+        @oem_link = 'http://www.realoem.com/bmw/enUS/showparts?id=VB13-USA-02_2004_E90_BMW_325i&diagId=41_1596#41357135679'
+        @ebay_cost = 51
+        @ebay_link = ''
         @side = 'left'
-        @link = 'http://www.realoem.com/bmw/enUS/showparts?id=VB13-USA-02_2004_E90_BMW_325i&diagId=41_1596#41357135679'
     end 
-
-
 end 
 
-class BodyNut < FrontFender
+class BodyNut < FrontSidePanel
     def initialize
-        @cost = 0.36 # USD
-        @weight = 0.004 # kg
         @type = 'M6'
+        @part = '07147133884'
+        @cost = 0.36
     end 
 end 
 
-class HexBoltWithWasher < FrontFender
+class HexBoltWithWasher < FrontSidePanel
     def initialize
-
+        @type = 'M6X12'
+        @part = '07147147513'
+        @cost = 0.38
     end 
 end 
