5a4b03b561dd72955388ba0a5859a0e53e44eb74
George V <georgevuong18@gmail.com>
2018-05-06 23:57:16 -0700

    update headlight and frontbumpercover class

--- car_repair.rb
+++ car_repair.rb
@@ -1,45 +1,45 @@
 # Car repair
 
-# Repair Estimate 1: Basic
+# Repair Estimate 1: Kim's Auto Body
 PARTS = {
     'front_bumper_cover_without_headlamp_washers' => {
-        'part_number' => 51117140859, 
+        'part_num' => 51117140859, 
         'estimate_cost' => 268
     },
     'front_fender' => {
-        'part_number' => 41357135679, 
+        'part_num' => 41357135679, 
         'estimate_cost' => 184
     },
     'left_headlamp_assembly_without_adaptive_headlamp' => {
-        'part_number' => 63117161665,
+        'part_num' => 63117161665,
         "estimate_cost" => 727
     }
 }.freeze
 
-# White Oaks Collision Center in Campbell, CA: Repair Estimate 2
+# Repair Estimate 2: White Oaks Collision Center in Campbell, CA 
 PARTS_TWO = {
     'front_bumper_cover' => {
-        'part_number' => '**QUALITY REPL PART', 
+        'part_num' => '**QUALITY REPL PART', 
         'estimate_cost' => 275, 
         'left_front_bumper_cover_support' => {
-            'part_number' => 51117058447,
+            'part_num' => 51117058447,
             'estimate_cost' => 7
         },
         'left_front_bumper_stiffener' => {
-            'part_number' => 51117134097,
+            'part_num' => 51117134097,
             'estimate_cost' => 8
         }
     },
     'front_fender' => {
-        'part_number' => "**QUALITY REPL PART", 
+        'part_num' => "**QUALITY REPL PART", 
         'estimate_cost' => 185,
         'left_fender_front_splash_shield' => 82
     },
     'left_front_combination_lamp_assembly' => {
-        'part_number' => 63117161669,
+        'part_num' => 63117161669,
         'estimate_cost' => 1343,
         'left_headlamp_bracket' => {
-            'part_number' => 51647116707,
+            'part_num' => 51647116707,
             'estimate_cost' => 62
         }
     }
@@ -57,7 +57,8 @@ CAR_MAKE_MODEL = {
     'door' => '4d',
     'liter' => 3,
     'cylinder' => 6,
-    'exterior_color' => 'gray',   
+    'exterior_color' => 'arctic metallic',
+    'paint_code' => 'a34',   
     'vin' => 'WBAVB13556PT08288',
     'market' => 'usa',
     'type_code' => 'vb13',
@@ -70,8 +71,12 @@ class FrontBumperCover
         @oem_cost = 410
         @oem_url = 'http://www.realoem.com/bmw/enUS/showparts?id=VB13-USA-02_2004_E90_BMW_325i&diagId=51_5813#51117140859'
     
-        @ebay_cost = 100
-        @ebay_url = 'https://www.ebay.com/sch/i.html?_odkw=51117140859+2006+BMW+325i+Base+3.0L&_dmd=1&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR0.TRC0.H0.X51117140859+2006+BMW+325i+Base+3.0L+gray.TRS0&_nkw=51117140859+2006+BMW+325i+Base+3.0L+gray&_sacat=0'
+        @ebay_cost = 274 # part and paint
+        @ebay_url = 'https://www.ebay.com/itm/NEW-Painted-to-Match-Front-Bumper-Cover-For-2006-2007-2008-BMW-3-Series/322253885207?hash=item4b07d40f17:g:SjsAAOSw1KxXMhIO&vxp=mtr'
+    
+        @local_cost = 400 # part and paint
+        @local_link = 'https://sfbay.craigslist.org/eby/pts/d/e90-pre-lci-mtech-or-m3-front/6577227515.html'
+        @dublin_link = 'http://www.dublin-collision.com/products---bumpers.html'
     end 
 end 
 
@@ -79,20 +84,14 @@ class HeadLight
     def initialize(side = 'left')
         @side = side
         @oem_cost = 940 # bi-xenon w/out adaptive headlamp
-        @oem_adapt_cost = 1100 # bi-xenon w/ adaptive headlamp
+        @oem_adapt_cost = 1100 # xenon w/ adaptive headlamp
         @oem_url = 'http://www.realoem.com/bmw/enUS/showparts?id=VB13-USA-02_2004_E90_BMW_325i&diagId=63_0907#63117161665'
 
-        # bi-xenon w/out adaptive headlamp
-        @ebay_cost = 600 
-        @ebay_url = 'https://www.ebay.com/sch/Headlights-/33710/i.html?_from=R40&_dcat=33710&Bulb%2520Type=Xenon&mvsub=1&_mcatda=true&selcontext=productType%3ACAR_AND_TRUCK&selfil=1%2C2%2C3%2C4%2C5&selvel=2006~BMW~325i~Base%2520Sedan%25204-Door~3.0L%25202996CC%2520l6%2520GAS%2520DOHC%2520Naturally%2520Aspirated~Base~3.0L&_nkw=63117161665%202006%20BMW%20325i%20Base%203.0L'
-        
-        # bi-xenon w/ adaptive headlamp
-        @ebay_adapt_cost = 780
-        @ebay_adapt_url = 'https://www.ebay.com/sch/i.html?_odkw=63117161665+2006+BMW+325i+Base+3.0L&Bulb%2520Type=Xenon&_dcat=33710&_osacat=33710&_from=R40&_trksid=p2045573.m570.l1313.TR0.TRC0.H0.X63117161669+2006+BMW+325i+Base+3.0L.TRS0&_nkw=63117161669+2006+BMW+325i+Base+3.0L&_sacat=33710'
+        # left xenon w/ adaptive headlamp
+        @ebay_cost = 430 
+        @ebay_url = 'https://www.ebay.com/itm/06-08-BMW-E90-323i-325i-328i-330i-335i-M3-LEFT-XENON-HID-ADAPTIVE-HEADLIGHT/332540309140?fits=Year%3A2006%7CMake%3ABMW%7CModel%3A325i&hash=item4d6cf26e94:g:-DEAAOSwy~BaKJN9&vxp=mtr'
     end 
 end 
-
-
 
 class FrontSidePanel 
     def initialize(side = 'left')
@@ -113,7 +112,7 @@ end
 class BodyNut
     def initialize
         @type = 'M6'
-        @part_number = '07147133884'
+        @part_num = '07147133884'
         @cost = 0.36
     end 
 end 
@@ -121,7 +120,7 @@ end
 class HexBoltWithWasher
     def initialize
         @type = 'M6X12'
-        @part_number = '07147147513'
+        @part_num = '07147147513'
         @cost = 0.38
     end 
 end 
