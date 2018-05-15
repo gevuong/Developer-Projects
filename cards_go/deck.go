c75f4c9e6e2022cf972acd70d0fec299898d95b9
George V <georgevuong18@gmail.com>
2018-05-15 08:05:11 -0700

    create custom deck type extends []string

---
+++ cards_go/deck.go
@@ -0,0 +1,4 @@
+package main
+
+// create a new type of 'deck', which is a slice of string
+type deck []string // this means that type deck === []string
--- cards_go/main.go
+++ cards_go/main.go
@@ -3,22 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	// create a new variable called card, where only a string will ever be assigned to this variable. This is important for statically-typed languages, like C++, Java, and Go. Dynamically-typed languages, such as JavaScript, Ruby, and Python, does not care what values we are assigning to variables.
-	// to initialize and assign variable, shorthand method would be to use := syntax.
-	card := newCard() // this is same as var card string = "Ace of Spades"
-	// Use := syntax ONLY when defining a new variable
+	// create a new variable called card, where only a string will ever be assigned to this variable. same as var card string = "Ace of Spades". // Use := syntax ONLY when defining a new variable
+	card := newCard()
 	fmt.Println(card)
 
-	cards := []string{"Ace of Diamonds", newCard()} // syntax to create a slice of type string
-	cards = append(cards, "Six of Spades")          // append() add ons a new string, does not modify existing string, but returns a new slice
+	// syntax to create a slice of type string
+	cards := deck{"Ace of Diamonds", newCard()}
 
-	// assign index and value of every element in cards slice to the variables of idx and card. We're using := syntax because for each iteration, we're basically throwing away previous idx and card that had been initialized and declared. So we need to re-initialize and re-declare those variables.
+	// append() add ons a new string, does not modify existing string, but returns a new slice
+	cards = append(cards, "Six of Spades")
+
+	// assign index and value of every element in cards slice to the variables, idx and card.
 	for idx, card := range cards {
 		fmt.Println(idx, card)
 	}
 }
 
-// need to tell Go exactly what data type is going to be returned. In this case, it would return a string. This means that whenever newCard() is called, it is going to return a value of type string.
+// need to tell Go exactly what data type function is going to return. In this case, it would return a string. This means that whenever newCard() is called, it is going to return a value of type string.
 func newCard() string {
 	return "Five of Diamonds"
 }
