e79e1a93a2fa4cc7ec9e1e9c7b04bb5b992089de
George V <georgevuong18@gmail.com>
2018-05-24 08:48:37 -0700

    practice parsing waypoints data

--- web_scraper_ruby/parse_waypoints.rb
+++ web_scraper_ruby/parse_waypoints.rb
@@ -1,44 +1,54 @@
 # need to convert coordinates to lat and long floats
 hash = Hash.new { |hash, key| hash[key] = {} }
-file = File.open("data/tallest_buildings_sf.tsv").each_with_index do |line, line_idx|
+File.open("data/tallest_buildings_sf.tsv").each_with_index do |line, line_idx|
     next if line_idx == 0
 
-    # line returns something like this: ["Salesforce Tower", "NULL", "1,070 (326)", "2017", "37°47′24″N,122°23′49″W"]
+    # line returns ["Salesforce Tower", "NULL", "1,070 (326)", "2017", "37°47′24″N,122°23′49″W"]
     line.strip.split("\t").each_with_index do |col_details, col_idx| 
         idx = line_idx - 1
+
         case col_idx 
         when 0
             hash[idx]["name"] = col_details 
-        when 2
-            hash[idx]["height"] = col_details
         when 3
             hash[idx]["year"] = col_details
         when 4
             hash[idx]["coordinates"] = col_details
         else 
-            next # skip "image" column because values are NULL
+            next # skip "image" and "height" column because values are NULL
         end
     end 
-    p hash
-
 end 
+p hash
+p hash[0]
 
-string = "37°47′24″N,122°23′49″W"
-lat = 0 
-long = 0
-string.split(",").each_with_index do |coord, coord_idx|
-    dms_coord = coord.gsub(/[°′″]/, ".").split(".") # returns ["37", "47", "24", "N"]
 
-    dms_coord.each_with_index do |dms, dms_idx|
-        case dms_idx, coord_idx
-        when 0 && 
-            dec
-    end 
-end 
+# string = "37°47′24″N,122°23′49″W"
+# lat = 0 
+# long = 0
+# string.split(",").each_with_index do |coord, coord_idx|
+#     dms_coord = coord.gsub(/[°′″]/, ".").split(".") # returns ["37", "47", "24", "N"]
 
-def convert_coord_to_decimal
+#     dms_coord.each_with_index do |dms, dms_idx|
+#         case 
+#         when (coord_idx == 0) && (dms_idx == 0) # lat && deg
+#             lat += dms.to_i 
+#         when (coord_idx == 0) && (dms_idx == 1) # lat && min
+#             lat += dms.to_i / 60.0
+#         when (coord_idx == 0) && (dms_idx == 2) # lat && sec 
+#             lat += dms.to_i / 3600.0
+#         when (coord_idx == 1) && (dms_idx == 0)
+#             long += dms.to_i
+#         when (coord_idx == 1) && (dms_idx == 1)
+#             long += dms.to_i / 60.0
+#         when (coord_idx == 1) && (dms_idx == 2)
+#             long += dms.to_i / 3600.0
+#         end 
+#     end
+# end 
 
-end 
+# p lat 
+# p long
 
 # tallest_buildings_hash = Hash.new
 # p headers.each do |header|
@@ -74,14 +84,12 @@ end
 landmarks = {
     1 => {
         "Title" => "Transverse and Column",
-        "Image" => "NULL",
         "Year" => "1977",
         "Location" => "Muni Barn, Corner of Ocean &amp; San Jose Ave,Curtis E. Green Metro Center",
         "Coordinates" => "NULL,NULL\r\n"
     },
     2 => {
         "Title" => "Transverse and Column",
-        "Image" => "NULL",
         "Year" => "1977",
         "Location" => "Muni Barn, Corner of Ocean &amp; San Jose Ave,Curtis E. Green Metro Center",
         "Coordinates" => "NULL,NULL\r\n"
