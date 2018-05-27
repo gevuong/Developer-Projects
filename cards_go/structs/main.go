2428cbf066201abd54d943198bb8efed20c1e5f0
George V <georgevuong18@gmail.com>
2018-05-27 12:17:49 -0700

    create struct with pointers

--- cards_go/structs/main.go
+++ cards_go/structs/main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-// custom type declarations person and
+// custom type declarations person and contactInfo
 type person struct {
-	firstName string
-	lastName  string
-	contact   contactInfo
+	firstName   string
+	lastName    string
+	contactInfo // equivalent to contactInfo contactInfo
 }
 
 type contactInfo struct {
@@ -14,17 +14,30 @@ type contactInfo struct {
 	zipCode int
 }
 
-// Third method to declare a struct to have zero values in place:
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 94123,
 		},
 	}
 
-	fmt.Println(jim)
-	fmt.Printf("%+v", jim)
+	// Use "&" operator to turn &jim into a memory address, or pointer, and assign it to jimPointer. that variable, jim, is located, and store in jimPointer. As a result, jimPointer is pointing to the memory address that the jim struct is located, not the value, jim.
+	jimPointer := &jim
+	// fmt.Println(jimPointer)
+	jimPointer.updateName("jimmy")
+	jim.print()
+}
+
+// pointerToPerson is the memory address that "jim" is located at. This gives direct access to the value that is stored in that memory address, which is jim struct.
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+// receiver fcn that accepts a receiver of type person, which is an extension of type struct
+func (p person) print() {
+	// fmt.Println(p)
+	fmt.Printf("%+v", p)
 }
