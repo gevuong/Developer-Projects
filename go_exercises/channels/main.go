8e26d384e1360a546c77cda0fdda000572345e57
George V <georgevuong18@gmail.com>
2018-05-29 15:05:41 -0700

    add Sleep statement in function literal and remove channel gotcha by creating a copy of range variable l using Gos pass-by-value

--- go_exercises/channels/main.go
+++ go_exercises/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -19,12 +20,22 @@ func main() {
 
 	// multiple go routines are uploaded or scheduled.
 	for _, link := range links {
-		go checkLink(link, c) // only use `go` keyword in front of function calls
+		go checkLink(link, c) // only use `go`keyword in front of function calls, adding go is a function call or invocation.
 	}
 
 	// each step, which is a value coming through c, is blocking call. This means that for each step, the main routine is waiting to receive a message from checkLink() after the http.Get() has completed. Feels a little hackish, but we are just printing messages that is received from every completed Go routine.
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	// alternative way to write loop: watch the channel c, whenever a value comes out of it, assign that value to l, and the body of the for loop is executed, which starts a checkLink go routine.
+	for l := range c {
+		// add a function literal (or anonymous function) and invocation (), to place the Sleep statement between each GET requests. We initially didn't want to place it in the main routine or delay the checkLink() with a timer. We decided to use a function literal (equivalent to an anonymous fcn or lambda in Ruby), which is a block of code that can be executed in the future. The tricky part is making sure you pass the l argument from outside the function literal scope, when invoking the function literal. Instead of passing l directly into the function literal, which may lead to issues.
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l) // extra set of parenthesis to invoke it, and pass l, which is a new copy in another memory address
+		// don't try to access the same variable from a child routine. We only share information to a child routine as an argument or from a channel
 	}
 }
 
@@ -33,10 +44,12 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link) // returns a Response struct and error type, but we are only concerned about error type, which states whether or not the site is up. Not so much the response to the request.
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
+		// c <- "Might be down I think"
 		return // make sure we don't do anything else inside the if statement
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep it's up"
+	c <- link
+	// c <- "Yep it's up"
 }
