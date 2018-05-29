9777150c3e5eab36f1c6f1106925ba9142967fe6
George V <georgevuong18@gmail.com>
2018-05-28 22:25:08 -0700

    create bot interface

---
+++ cards_go/interfaces/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+// create a custom type "bot", where if you are a type with a fcn getGreeting() that returns a string, then you are a member of type "bot". Because you are a member of type "bot", you can call printGreeting(). We used interface to define what functions and return values it should have.
+type bot interface {
+	getGreeting() string
+}
+
+// interface syntax
+// type interfaceName interface {
+// 	getGreeting(input list of argument types) (input list of return types)
+// }
+
+// i.e.
+// type bot interface {
+// 	getGreeting(string, int) (string, error)
+// }
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// can remove receiver variable if it's not being used i.e. (eb englishBot) -> (englishBot)
+func (englishBot) getGreeting() string {
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola hay!"
+}
